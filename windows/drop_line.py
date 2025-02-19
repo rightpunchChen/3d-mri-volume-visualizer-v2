a1e3822ae66e92f63e8867921d3a92f3c1b4a8ee
Charles Chen <rightpunch1123@gmail.com>
2025-02-19 12:06:46 +0800

    Update drop_line.py

--- windows/drop_line.py
+++ windows/drop_line.py
@@ -4,9 +4,10 @@ from PySide6.QtWidgets import QLineEdit
 class DropLineEdit(QLineEdit):
     textDropped = Signal(str)
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, file_type=('.nii', '.nii.gz')):
         super().__init__(parent)
         self.setAcceptDrops(True)
+        self.file_type = file_type
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -15,7 +16,7 @@ class DropLineEdit(QLineEdit):
     def dropEvent(self, event):
         for url in event.mimeData().urls():
             file_path = url.toLocalFile()
-            if file_path.endswith(('.nii', '.nii.gz')):
+            if file_path.endswith(self.file_type):
                 self.setText(file_path)
                 self.textDropped.emit(file_path)
                 break
